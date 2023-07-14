f75409cd5146f806ee33ef11b68035cec2b14cc3
rafiwts <rafiwts@gmail.com>
2023-07-14 23:16:51 +0200

    Rearanging a main file

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
-from .discord_bot.client import 
-from settings import CHANNEL_ID, GUILD, 
+import discord
+
+import logging
+
+from bot.client import DiscordBot
+from bot.session import Session
+from bot.server_events import ServerEvents
+from bot.users_commands import UserCommands
+from settings import CHANNEL_ID, GUILD, TOKEN
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+handler = logging.FileHandler(filename='discord.log',
+                        encoding='utf-8',
+                        mode='w')
+
+bot = DiscordBot(command_prefix='!', intents=intents)
+
+bot.initialize()
+
+new_session = Session(bot_id=bot.user,
+                        user_id=bot.user)  
+
 
 @bot.event
 async def on_ready() -> None:
       response_to_ready = ServerEvents.return_on_ready(bot=bot, 
-                                                       channel_id=CHANNEL_ID, 
-                                                       guild=GUILD)
+                                                      channel_id=CHANNEL_ID, 
+                                                      guild=GUILD)
       await response_to_ready
 
 
 @bot.event
 async def on_member_join(member):
       response_to_joining = ServerEvents.return_on_joining(member=member,
-                                                           channel=CHANNEL_ID)                                          
+                                                      channel=CHANNEL_ID)                                          
       await response_to_joining
 
 
 @bot.event
 async def on_member_remove(member):
       response_to_removing = ServerEvents.return_on_removing(member=member,
-                                                             channel=CHANNEL_ID)
+                                                            channel=CHANNEL_ID)
       await response_to_removing
 
 
 @bot.event
-async def on_message(message) -> None:
+async def on_message(message: discord.Message) -> None:
       #TODO: different events -> and different commands - bot should display them - we will develop it later on
       #TODO: Think about a more efficient implementation of the list
       list_of_commands = ['$', '?', 'showevents', 'showcommands']
@@ -35,9 +58,10 @@ async def on_message(message) -> None:
       if message.content in list_of_commands:
             response_to_message = ServerEvents.return_on_message(message=message)
             await response_to_message
-
+            await bot.process_message(message)
+            
       await bot.process_commands(message)
-
+ 
 
 @bot.event
 async def on_message_edit(old_message, new_message):
@@ -56,15 +80,15 @@ async def on_message_delete(message):
 
 @bot.event
 async def on_typing(channel, user, when):
-     response_to_typing = ServerEvents.return_on_typing(channel=channel, 
-                                                        user=user, 
-                                                        when=when)
-     await response_to_typing
+      response_to_typing = ServerEvents.return_on_typing(channel=channel, 
+                                                            user=user, 
+                                                            when=when)
+      await response_to_typing
 
 
 @bot.command()
 async def joined(ctx, *, member: discord.Member):
-    await ctx.send(f'{member} joined on {member.joined_at}')
+      await ctx.send(f'{member} joined on {member.joined_at}')
 
 
 @bot.command()
@@ -75,7 +99,7 @@ async def start(ctx):
             return
       
       start_new_session = UserCommands.new_session_command(context=ctx,
-                                                           new_session=new_session)
+                                                      new_session=new_session)
       await start_new_session
 
 
@@ -86,42 +110,42 @@ async def session(ctx):
             return
 
       duration_of_session = UserCommands.lasting_session(context=ctx,
-                                                         current_session=new_session)
+                                                      current_session=new_session)
       await duration_of_session
 
 
 @bot.command()
 async def end(ctx):
       if not new_session.is_active:
-          await ctx.send('Session is not active!')
-          return
-     
+            await ctx.send('Session is not active!')
+            return
+
       end_current_session = UserCommands.end_session_command(context=ctx,
-                                                             current_session=new_session)
+                                                            current_session=new_session)
       await end_current_session
 
 
 @bot.command()
 async def users(ctx):
       users_list = UserCommands.list_of_users(context=ctx,
-                                              guild_id=GUILD,
-                                              bot=bot)
+                                          guild_id=GUILD,
+                                          bot=bot)
       await users_list
 
 
 @bot.command()
 async def square(ctx, arg):
       return_square_number = UserCommands.return_square(context=ctx,
-                                                        users_choice=arg)
+                                                      users_choice=arg)
       await return_square_number
 
       
 @bot.command()
 async def scrabblepoints(ctx, arg):
       return_points = UserCommands.get_scrabble_points(context=ctx,
-                                                       users_choice=arg)
+                                                      users_choice=arg)
       await return_points
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
       bot.run(TOKEN, log_handler=handler)

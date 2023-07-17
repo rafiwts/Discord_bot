387e750156f82e437489ff7ebdc119c8d4f102ef
rafiwts <rafiwts@gmail.com>
2023-07-17 23:42:07 +0200

    Adding controller for

---
+++ bot/event_controller.py
@@ -0,0 +1,11 @@
+import discord
+
+class Controller:
+    async def message_controller(self, message: discord.Message) -> None:
+        print(message)
+
+    async def reaction_controller(self, reaction: discord.RawReactionActionEvent) -> None:
+        print(reaction)
+    
+    async def command_controller(self, message: discord.Message) -> None:
+        print(message)

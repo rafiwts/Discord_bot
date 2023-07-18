77d1745072dace11ce109be2d077a3ce37bd00e2
rafiwts <rafiwts@gmail.com>
2023-07-18 23:25:34 +0200

    New environment variables for the database

--- settings.py
+++ settings.py
@@ -8,4 +8,9 @@ TOKEN = os.getenv('TOKEN')
 #FIXME: does not work as an environment variable
 CHANNEL_ID = 1125323466921476118 
 GUILD = os.getenv('DISCORD_GUILD')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+USER_NAME = os.getenv('USER_NAME')
+PASSWORD = os.getenv('PASSWORD')
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
 BASE_DIR = pathlib.Path(__file__).parent

8a6cda0c5b697609924824915d3c9b9272a4b5a6
rafiwts <rafiwts@gmail.com>
2023-07-25 23:55:35 +0200

    Changing columns in a database reaction

--- database/models/reaction.py
+++ database/models/reaction.py
@@ -6,16 +6,17 @@ from database.models import DiscordUser, Message
 
 
 class Reaction(DefaultDatabaseModel):
-    user: int = peewee.ForeignKeyField(DiscordUser, 
-                                       backref='reactions',
-                                       on_delete='CASCADE')
-    content = peewee.ForeignKeyField(Message,
+    user_from: int = peewee.ForeignKeyField(DiscordUser, 
+                                            backref='senders',
+                                            on_delete='CASCADE')
+    user_to: int = peewee.ForeignKeyField(DiscordUser,
+                                          backref='recipients',
+                                          on_delete='CASCADE')
+    message = peewee.ForeignKeyField(Message,
                                      backref='reactions',
                                      on_delete='CASCADE')
     added_at: int = peewee.DateTimeField(default=datetime.now)
     edited_at: int = peewee.DateTimeField(default=datetime.now)
-    reaction_counter: int = peewee.IntegerField(default=0)
    
-    
     def __str__(self) -> str:
         return f'Message {self.id}'

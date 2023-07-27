16736314f8b68b2618985455fb6e8be59dee343a
rafiwts <rafiwts@gmail.com>
2023-07-27 23:58:14 +0200

    Changing columns in a bot table

--- database/models/user.py
+++ database/models/user.py
@@ -9,9 +9,10 @@ class DiscordUser(DefaultDatabaseModel):
                                      null=False,
                                      unique=True)
     guildname: str = peewee.CharField(max_length=50)
-    created_at: int = peewee.DateTimeField(default=datetime.now)
-    joined_at: int = peewee.DateTimeField(default=datetime.now)
-    is_bot: bool = peewee.BooleanField()
+    created_at: int = peewee.DateTimeField(default=datetime.now())
+    joined_at: int = peewee.DateTimeField(default=datetime.now())
+    is_bot: bool = peewee.BooleanField(default=False)
+    is_admin: peewee.BooleanField = peewee.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.username

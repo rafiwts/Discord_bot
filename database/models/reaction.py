4aeeb73c65836b0dc5dca1a4e00a1327c1aa88bd
rafiwts <rafiwts@gmail.com>
2023-08-03 22:44:14 +0200

    Custom method for creating new reaction

--- database/models/reaction.py
+++ database/models/reaction.py
@@ -12,11 +12,20 @@ class Reaction(DefaultDatabaseModel):
     user_to: int = peewee.ForeignKeyField(DiscordUser,
                                           backref='recipients',
                                           on_delete='CASCADE')
-    message = peewee.ForeignKeyField(Message,
-                                     backref='reactions',
+    message:int = peewee.ForeignKeyField(Message,
+                                     to_field='message_id'
+                                     backref='reactions'
                                      on_delete='CASCADE')
-    added_at: int = peewee.DateTimeField(default=datetime.now)
-    edited_at: int = peewee.DateTimeField(default=datetime.now)
+    added_at: int = peewee.DateTimeField(default=datetime.now())
+    edited_at: int = peewee.DateTimeField(default=datetime.now())
    
+    @classmethod
+    def create_new_reaction(cls, user_from: peewee.ForeignKeyField,
+                                 user_to: peewee.ForeignKeyField, 
+                                 message: peewee.ForeignKeyField):
+        return cls.create(user_from=user_from,
+                          user_to=user_to,
+                          message=message)
+    
     def __str__(self) -> str:
         return f'Message {self.id}'

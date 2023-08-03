e1119a2fef6428a62b81155fdf3846f60e756134
rafiwts <rafiwts@gmail.com>
2023-08-03 22:43:54 +0200

    Custom method for editing messages

--- database/models/message.py
+++ database/models/message.py
@@ -4,30 +4,42 @@ from datetime import datetime
 from database.models.base import DefaultDatabaseModel
 from database.models import DiscordUser
 #FIXME: cannot import from the 'init' file
-from database.models.command import Command
+from database.models.command import Command, DiscordUser
 
 
 class Message(DefaultDatabaseModel):
+    message_id = peewee.BigIntegerField(primary_key=True)
     content: str = peewee.TextField(null=False)
-    created_at: int = peewee.DateTimeField(default=datetime.now)
+    created_at: int = peewee.DateTimeField(default=datetime.now())
     edited_at: int = peewee.DateTimeField(null=True)
     user: int = peewee.ForeignKeyField(DiscordUser,
                                        backref='messages',
                                        on_delete='CASCADE')
     command: int = peewee.ForeignKeyField(Command,
                                           backref='messages',
-                                          on_delete='CASCADE')
+                                          on_delete='SET NULL',
+                                          null=True)
     reaction_counter: int = peewee.IntegerField(default=0)
 
     @classmethod
-    def create_new_message(cls, content: peewee.TextField,
+    def create_new_message(cls, message_id: peewee.BigIntegerField,
+                                content: peewee.TextField,
                                 first_created: peewee.DateTimeField, 
                                 user: peewee.ForeignKeyField,
-                                command: peewee.ForeignKeyField):
-        return cls.create(content=content,
+                                command: peewee.ForeignKeyField = None):
+        return cls.create(message_id=message_id,
+                          content=content,
                           created_at=first_created,
                           user=user,
                           command=command)
     
+    @classmethod
+    def edit_message(cls, message_id: peewee.BigIntegerField,
+                          new_content: peewee.TextField, 
+                          edited_at: peewee.DateTimeField):
+        
+        return cls.update(content=new_content,
+                          edited_at=edited_at).where(cls.message_id==message_id).execute()
+ 
     def __str__(self) -> str:
         return f'Message {self.id}'

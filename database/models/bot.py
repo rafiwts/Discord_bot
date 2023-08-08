11925669865c544c25f2c75cd9124c739c780a90
rafiwts <rafiwts@gmail.com>
2023-08-08 22:38:51 +0200

    Adding a method checking if user in database

--- database/models/bot.py
+++ database/models/bot.py
@@ -1,10 +1,12 @@
 import peewee
+from discord.ext import commands
 
 from database.models.base import DefaultDatabaseModel
 from database.models import DiscordUser
 
 
 class BotUser(DefaultDatabaseModel):
+    discord_id: peewee.BigIntegerField = peewee.BigIntegerField()
     botname: peewee.CharField = peewee.CharField(max_length=50, 
                                                  null=False,
                                                  unique=True)
@@ -14,12 +16,19 @@ class BotUser(DefaultDatabaseModel):
                                                               on_delete='CASCADE')
     
     @classmethod
-    def create_new_bot(cls, botname: peewee.CharField, 
-                            command_prefix: peewee.CharField, 
+    def create_new_bot(cls, bot: commands.Bot, 
                             owner_id: peewee.ForeignKeyField):
-        return cls.create(botname=botname,
-                          command_prefix=command_prefix,
+        return cls.create(discord_id=bot.application_id,
+                          botname=bot.user,
+                          command_prefix=bot.command_prefix,
                           owner_id=owner_id)
 
+    @classmethod
+    def check_user_or_bot(cls, discord_id: peewee.BigIntegerField):
+        try:
+            return DiscordUser.get(discord_id=discord_id)
+        except DiscordUser.DoesNotExist:
+            return cls.get(discord_id=discord_id)
+        
     def __str__(self) -> str:
         return self.botname

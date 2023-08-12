dd62888c79b2a0b3cf22b0f3bdb1a2aa47181055
rafiwts <rafiwts@gmail.com>
2023-08-12 23:29:20 +0200

    Add table column and logic for handling bans

--- database/models/user.py
+++ database/models/user.py
@@ -1,10 +1,12 @@
 import peewee
+import discord
 from datetime import datetime
 
 from database.models.base import DefaultDatabaseModel
 
 
 class DiscordUser(DefaultDatabaseModel):
+    discord_id = peewee.BigIntegerField()
     username: str = peewee.CharField(max_length=50, 
                                      null=False,
                                      unique=True)
@@ -13,31 +15,60 @@ class DiscordUser(DefaultDatabaseModel):
     joined_at: int = peewee.DateTimeField(default=datetime.now())
     is_bot: bool = peewee.BooleanField(default=False, null=False)
     is_admin: peewee.BooleanField = peewee.BooleanField(default=False, null = False)
+    banned: peewee.BooleanField = peewee.BooleanField(default=False, null = False)
 
     @classmethod
-    def create_new_user(cls, username: peewee.CharField, 
-                             guildname: peewee.CharField,
-                             created_at: peewee.DateTimeField,
-                             joined_at: peewee.DateTimeField):
-        return cls.create(username=username,
+    def create_new_user(cls, discord_user: discord.Member,
+                             guildname: peewee.CharField,):
+        return cls.create(discord_id=discord_user.id,
+                          username=discord_user.name,
                           guildname=guildname,
-                          created_at=created_at)
+                          created_at=discord_user.created_at)
     
     @classmethod
-    def create_new_admin(cls, username: peewee.CharField, 
+    def get_or_create_user(cls, sender: discord.Member):
+        discord_user = cls.get_or_none(discord_id=sender.id)
+        if discord_user is None:
+            discord_user =  cls.create_new_user(discord_id=sender.id,
+                                                username=sender.name,
+                                                guildname=sender.guild,
+                                                created_at=sender.created_at,
+                                                joined_at=sender.joined_at)
+
+            discord_user.save()
+        
+        return discord_user
+
+    @classmethod
+    def create_new_admin(cls, bot_owner: discord.Member, 
                               guildname: peewee.CharField,
-                              created_at: peewee.DateTimeField,
                               admin: peewee.BooleanField = True):
-        return cls.create(username=username,
+        return cls.create(discord_id=bot_owner.id,
+                          username=bot_owner.name,
                           guildname=guildname,
-                          created_at=created_at,
+                          created_at=bot_owner.created_at,
                           is_admin=admin)
     
     @classmethod
-    def update_user_to_admin(cls, username: peewee.CharField, 
+    def update_user_to_admin(cls, discord_id: peewee.CharField, 
                                   admin: peewee.BooleanField = True):
-        cls.update(is_admin=admin).where(cls.username==username).execute()
-        return cls.get(username==cls.username)
+        cls.update(is_admin=admin).where(cls.discord_id==discord_id).execute()
+        return cls.get(discord_id==cls.discord_id)
+
+    @classmethod
+    def update_user_data(cls, discord_user: discord.Member,
+                              guild: discord.Guild,
+                              updated_user: discord.Member = None,
+                              banned: bool = None):
+        if banned is None:
+            cls.update(username=updated_user.name,
+                       guildname=guild)\
+                .where(cls.discord_id==discord_user.id)\
+                .execute()
+        else:
+            cls.update(banned=banned)\
+               .where(cls.discord_id==discord_user.id)\
+               .execute()
 
     def __str__(self) -> str:
         return self.username
